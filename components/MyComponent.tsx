3860b4f770633ca8eea5b33c19fb41ac7752dbe5
KATT <alexander@n1s.se>
2017-09-23 21:35:02 +0100

    :tada: init
    https://github.com/zeit/next.js/blob/master/examples/with-typescript

---
+++ components/MyComponent.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default () => (
+  <div>
+    <p>This is my component</p>
+  </div>
+);
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import MyComponent from '../components/MyComponent';
+
+export default () => (
+  <div>
+    <h1>Hello world</h1>
+    <MyComponent />
+  </div>
+);
