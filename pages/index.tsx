c34366f225d20487e32816d5ac82fb26002492a6
KATT <alexander@n1s.se>
2017-09-23 22:10:09 +0100

    :sparkles: meow world, async fetch data.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
 import * as React from 'react';
 import MyComponent from '../components/MyComponent';
+import 'isomorphic-fetch';
 
-export default () => (
-  <div>
-    <h1>Hello world</h1>
-    <MyComponent />
-  </div>
-);
+interface IndexPageProps {
+  text: string;
+}
+
+export default class IndexPage extends React.Component<IndexPageProps, {}> {
+  static async getInitialProps({ req }) {
+    const baseUrl = req ? `${req.protocol}://${req.headers.host}` : '';
+
+    const res = await fetch(baseUrl + '/api/cat/speak');
+    const text = await res.text();
+
+    return { text };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.text} world</h1>
+        <MyComponent />
+      </div>
+    );
+  }
+}
---
+++ server.ts
@@ -0,0 +1,25 @@
+// server.ts
+import * as express from 'express';
+import * as next from 'next';
+
+const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get('/api/cat/speak', (req, res) => {
+    res.send('meow');
+  });
+
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
