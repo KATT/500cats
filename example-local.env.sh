7547a4bbf2b6210470f8cc1013c80302f8ec9b71
KATT <alexander@n1s.se>
2017-09-26 01:09:49 +0100

    :books: example

---
+++ example-local.env.sh
@@ -0,0 +1,21 @@
+export S3_ACCESS_KEY_ID='xxxxx'
+export S3_SECRET_ACCESS_KEY='xxxxxxx'
+export S3_REGION='eu-west-2'
+export S3_BUCKET='500cats'
+
+export PG_MAIN_HOST='horton.elephantsql.com'
+export PG_MAIN_USER='xxxxxx'
+export PG_MAIN_DB='xxxxxx'
+export PG_MAIN_PASSWORD='xxxxxx'
+
+# for deployment, run this:
+
+# now secret add S3_ACCESS_KEY_ID='xxxxx'
+# now secret add S3_SECRET_ACCESS_KEY='xxxxxxx'
+# now secret add S3_REGION='eu-west-2'
+# now secret add S3_BUCKET='500cats'
+
+# now secret add PG_MAIN_HOST='horton.elephantsql.com'
+# now secret add PG_MAIN_USER='xxxxxx'
+# now secret add PG_MAIN_DB='xxxxxx'
+# now secret add PG_MAIN_PASSWORD='xxxxxx'
