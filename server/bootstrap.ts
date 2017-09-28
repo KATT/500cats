944660cf71e31682ecdc4d4477bef9481287472b
KATT <alexander@n1s.se>
2017-09-28 14:06:41 +0100

    :zap: add all the cats

--- server/bootstrap.ts
+++ server/bootstrap.ts
@@ -1,19 +1,44 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+function shuffle<T>(array: Array<T>): Array<T> {
+  let currentIndex: number = array.length,
+    temporaryValue: T,
+    randomIndex: number;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 const dir = path.join(__dirname, '/__bootstrap__');
-const images = fs
+let images = fs
   .readdirSync(dir)
   .filter(file => fs.statSync(path.join(dir, file)).isFile())
   .filter(file => file.endsWith('.jpg'))
-  .slice(0, 100);
+  .sort();
+images = shuffle(images);
 
-import { models } from './models';
+import { models, sequelize } from './models';
 
 async function main() {
+  console.log('Deleting everything..');
+  await sequelize.sync({ force: true });
+
+  console.log('Uploading cats!');
   for (const index in images) {
-    console.log(index + '...');
     const image = images[index];
+    console.log(`${Number(index) + 1}/${images.length}\t - ${image}...`);
 
     await models.Cat.createFromFile(image, `${dir}/${image}`);
   }
